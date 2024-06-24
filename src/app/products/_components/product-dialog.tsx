eeaab07996fdf1712ad49db7324c27aff683241d
gabrielmulleer <emaildegabrielm@gmail.com>
2024-06-23 21:40:36 -0300

    feat: add 'Add Product' button in products page

---
+++ src/app/products/_components/product-dialog.tsx
@@ -0,0 +1,146 @@
+'use client'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Switch } from '@/components/ui/switch'
+import { useRef } from 'react'
+import { useRouter } from 'next/navigation'
+import { registerProductSchema } from '@/app/(main)/schema'
+import { createProduct } from '@/app/(main)/actions'
+
+export function ProductDialog() {
+  const ref = useRef<HTMLDivElement>(null)
+  const router = useRouter()
+  const form = useForm<z.infer<typeof registerProductSchema>>({
+    resolver: zodResolver(registerProductSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      price: 0,
+      available: false,
+    },
+  })
+  const onSubmit = form.handleSubmit(
+    async (data: z.infer<typeof registerProductSchema>) => {
+      await createProduct(data)
+      form.reset()
+      router.refresh()
+      ref.current?.click()
+    },
+  )
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Cadastrar Produto</Button>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Cadastrar produto</DialogTitle>
+          <DialogDescription>
+            Insira as informações do novo produto. Clique em adicionar ao
+            finalizar.
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={onSubmit} className="space-y-8">
+            <div className="grid gap-4 py-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel className="text-right">Nome</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="col-span-3"
+                        placeholder="Escreva seu nome"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel className="text-right">Descrição</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="col-span-3"
+                        placeholder="Escreva a descrição do produto"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4">
+                    <FormLabel className="text-right">Preço</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="col-span-3"
+                        type="number"
+                        placeholder="Escreva o preço do produto"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="available"
+                render={({ field }) => (
+                  <FormItem className="grid grid-cols-4 items-center gap-4 ">
+                    <FormLabel className="text-right">
+                      Disponível para venda
+                    </FormLabel>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <DialogFooter className="mt-auto">
+              <Button disabled={form.formState.isLoading} type="submit">
+                {form.formState.isSubmitting
+                  ? 'Adicionando...'
+                  : 'Adicionar produto'}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
