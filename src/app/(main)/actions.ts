436433c70634f120f336274ee8ef717e7970fa49
gabrielmulleer <emaildegabrielm@gmail.com>
2024-06-23 21:41:06 -0300

    fix: API requests

--- src/app/(main)/actions.ts
+++ src/app/(main)/actions.ts
@@ -1,6 +1,8 @@
 'use server'
 import { z } from 'zod'
 import { registerProductSchema } from './schema'
+import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache'
 
 const baseUrl = process.env.NEXT_PUBLIC_APP_URL
 
@@ -19,15 +21,9 @@ export async function createProduct(
   const data = await response.json()
   if (response.ok) {
     console.log('Product saved successfully')
+    revalidatePath('/products')
+    redirect('/products')
   } else {
     console.error('Error saving product: ' + data.error)
   }
-}
-
-export async function getProducts() {
-  const response = await fetch(`${baseUrl}/api/products`, {
-    method: 'GET',
-  })
-  const data = await response.json()
-  return data
 }
